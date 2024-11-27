b6fe0c8601af1227a1ee12843e7ac44604543f97
Frantisek Gejdos <gejdos@alos.gg>
2024-11-27 16:35:23 +0100

    constructors update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ module.exports = {
   getRequestInfo: require('./getRequestInfo'),
   detectDevTools: require('./detectDevTools'),
   expressLogger: require('./expressLogger'),
-  RateLimiter: require('./rateLimiter')
+  RateLimiter: require('./rateLimiter'),
+  WebSocketSupport: require('./websocketSupport'),
+  GraphQLProfiler: require('./graphqlProfiler'),
+  MetricsDashboard: require('./metricsDashboard')
 };
